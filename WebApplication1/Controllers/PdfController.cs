2f5c131cb6bc2c008cc262bde01412d9af8d7286
akivakn <akivakaufman@gmail.com>
2024-06-28 14:38:00 +0100

    feat: allow user to print dish to pdf
    Add pdf controller to create pdf from html string. Add print button to dish card to send html string to pdf controller excluding buttons and links.

---
+++ WebApplication1/Controllers/PdfController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelectPdf;
+
+namespace MyRecipes.Controllers
+{
+    public class PdfController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(string htmlString)
+        {
+            HtmlToPdf converter = new HtmlToPdf();
+            PdfDocument doc = converter.ConvertHtmlString(htmlString);
+            var pdfBytes = doc.Save();
+
+            return File(pdfBytes,"application/pdf");
+        }
+    }
+}
